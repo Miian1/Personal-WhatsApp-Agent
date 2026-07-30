664e09a6c2b05e46cedaa99dba1e882a397bd979
personnall <mypcaccc01@gmail.com>
2026-07-30 09:31:33 -0700

    fix: enable trust proxy for Vercel reverse proxy

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ const whatsappController = require('./controllers/whatsappController');
 const knowledgeService = require('./services/knowledgeService');
 
 const app = express();
+app.set('trust proxy', 1);
 
 app.use(helmet());
 app.use(cors());
